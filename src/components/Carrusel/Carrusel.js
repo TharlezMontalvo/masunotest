c07a2d20fe86503782b83e59a7b005f2dccd82fc
TharlezMontalvo <75101071+TharlezMontalvo@users.noreply.github.com>
2024-01-26 19:27:46 -0500

    Agregando boton indice al carrusel

--- src/components/Carrusel/Carrusel.js
+++ src/components/Carrusel/Carrusel.js
@@ -13,27 +13,36 @@ const Carrusel = () => {
   };
 
   const prevSlide = () => {
-    console.log('Prev Slide Clicked');
     setCurrentIndex((prevIndex) => (prevIndex - 1 + 3) % 3);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
   };
 
   return (
     <div className="carousel">
-      <button className={`arrow-button prev ${currentIndex === 0 ? 'hidden' : ''}`} onClick={prevSlide}>&#8249;</button>
+      <button className={`arrow-button prev ${currentIndex === 0 ? 'hidden' : ''}`} onClick={prevSlide}>
+        &#8249;
+      </button>
 
       <div className="carousel-container" style={{ transform: `translateX(${-currentIndex * 100}%)`, transition: 'transform 0.5s ease-in-out' }}>
         <div className={`carousel-item ${currentIndex === 0 ? 'active' : ''}`}>
           <div className="column left">
-            
             <div className="text-content">
               <img src={star} alt="Imagen 1" className="star-image" />
               <h2 className="title">PHP</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <button className="ver-button">Ver Tecnología</button>
             </div>
+            <div className="slide-indicators">
+              <span className={`indicator ${currentIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}></span>
+              <span className={`indicator ${currentIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}></span>
+              <span className={`indicator ${currentIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}></span>
+            </div>
           </div>
           <div className="column right">
-            <img src={slide1} alt="Imagen predefinida" className="slide-image"/>
+            <img src={slide1} alt="Imagen predefinida" className="slide-image" />
           </div>
         </div>
 
@@ -45,9 +54,14 @@ const Carrusel = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <button className="ver-button">Ver Tecnología</button>
             </div>
+            <div className="slide-indicators">
+              <span className={`indicator ${currentIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}></span>
+              <span className={`indicator ${currentIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}></span>
+              <span className={`indicator ${currentIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}></span>
+            </div>
           </div>
           <div className="column right">
-            <img src={slide2} alt="Imagen predefinida" className="slide-image"/>
+            <img src={slide2} alt="Imagen predefinida" className="slide-image" />
           </div>
         </div>
 
@@ -59,14 +73,21 @@ const Carrusel = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <button className="ver-button">Ver Tecnología</button>
             </div>
+            <div className="slide-indicators">
+              <span className={`indicator ${currentIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}></span>
+              <span className={`indicator ${currentIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}></span>
+              <span className={`indicator ${currentIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}></span>
+            </div>
           </div>
           <div className="column right">
-            <img src={slide3} alt="Imagen predefinida" className="slide-image"/>
+            <img src={slide3} alt="Imagen predefinida" className="slide-image" />
           </div>
         </div>
       </div>
 
-      <button className={`arrow-button next ${currentIndex === 2 ? 'hidden' : ''}`} onClick={nextSlide}>&#8250;</button>
+      <button className={`arrow-button next ${currentIndex === 2 ? 'hidden' : ''}`} onClick={nextSlide}>
+        &#8250;
+      </button>
     </div>
   );
 };
